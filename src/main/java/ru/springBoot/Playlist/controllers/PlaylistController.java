e29dbdf1d0d577adc25d3b37c0230ac40ffebf14
artemowork <artem180791@mail.ru>
2023-06-30 23:12:02 +0300

    Настройка контроллера PlaylistController - настроин поиск и отображение песен для прослушивания

--- src/main/java/ru/springBoot/Playlist/controllers/PlaylistController.java
+++ src/main/java/ru/springBoot/Playlist/controllers/PlaylistController.java
@@ -1,16 +1,49 @@
 package ru.springBoot.Playlist.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.springBoot.Playlist.models.Author;
+import ru.springBoot.Playlist.services.AuthorServices;
+import ru.springBoot.Playlist.services.SongServices;
+
+import java.util.List;
 
 @Component
 @RequestMapping("/index")
 public class PlaylistController {
 
+    private final AuthorServices authorServices;
+    private final SongServices songServices;
+
+    @Autowired
+    public PlaylistController(AuthorServices authorServices, SongServices songServices) {
+        this.authorServices = authorServices;
+        this.songServices = songServices;
+    }
+
     @GetMapping
     public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("{id}")
+    public String oneAuthor(@PathVariable("id") int id, Model model){
+        model.addAttribute("author", authorServices.findOneAuthor(id));
+        model.addAttribute("songs", authorServices.getSongsByAuthorId(id));
+        return "index_play";
+    }
+
+    @GetMapping("/search")
+    public String searchAuthor(@RequestParam("searchString") String searchAuthor, Model model){
+        model.addAttribute("search", searchAuthor);
+        model.addAttribute("authors", authorServices.findAllAuthor(searchAuthor));
+        List<Author> authorList = authorServices.findAllAuthor(searchAuthor);
+
         return "index";
     }
 
