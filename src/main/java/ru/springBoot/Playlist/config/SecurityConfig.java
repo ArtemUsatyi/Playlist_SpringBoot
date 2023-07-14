13624ce8010880d899d1f732265f7c12c67e39a1
artemowork <artem180791@mail.ru>
2023-07-14 14:57:49 +0300

    Настройка доступа к страничкам и авторизация и разлогирования

--- src/main/java/ru/springBoot/Playlist/config/SecurityConfig.java
+++ src/main/java/ru/springBoot/Playlist/config/SecurityConfig.java
@@ -3,28 +3,48 @@ package ru.springBoot.Playlist.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.springBoot.Playlist.services.UsernameDetailsServices;
+import ru.springBoot.Playlist.services.AccountDetailsServices;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UsernameDetailsServices usernameDetailsServices;
+    private final AccountDetailsServices accountDetailsServices;
 
     @Autowired
-    public SecurityConfig(UsernameDetailsServices usernameDetailsServices) {
-        this.usernameDetailsServices = usernameDetailsServices;
+    public SecurityConfig(AccountDetailsServices accountDetailsServices) {
+        this.accountDetailsServices = accountDetailsServices;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/auth/registration", "/error", "/index", "/index/about", "/index/search", "/index/{id}").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/adminPanel", true)
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/index");
     }
 
     // Насраивает аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usernameDetailsServices);
+        auth.userDetailsService(accountDetailsServices);
     }
+
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
