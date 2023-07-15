e09c984f754077bab86c9e96390eec282061d68f
artemowork <artem180791@mail.ru>
2023-07-15 20:00:37 +0300

    Добавлен переход на страничку 403 Доступ Запрещен

--- src/main/java/ru/springBoot/Playlist/config/SecurityConfig.java
+++ src/main/java/ru/springBoot/Playlist/config/SecurityConfig.java
@@ -3,15 +3,17 @@ package ru.springBoot.Playlist.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.springBoot.Playlist.services.AccountDetailsServices;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AccountDetailsServices accountDetailsServices;
@@ -23,19 +25,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/login", "/auth/registration", "/error", "/index", "/index/about", "/index/search", "/index/{id}").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/auth/login", "/auth/registration", "/error", "/index", "/index/search", "/index/{id}").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
-                .defaultSuccessUrl("/adminPanel", true)
+                .defaultSuccessUrl("/index", true)
                 .failureUrl("/auth/login?error")
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/index");
+                .logoutSuccessUrl("/index")
+                .and()
+                .exceptionHandling().accessDeniedPage("/user/403");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/css/**", "/images/**", "/js/**");
     }
 
     // Насраивает аутентификацию
