8eba231461152efe0522e696f80efa8ae5e8a3c8
artemowork <artem180791@mail.ru>
2023-07-18 19:52:02 +0300

    Добавления отображения страничек пагинации в плейлисте Песен

--- src/main/java/ru/springBoot/Playlist/controllers/PlaylistController.java
+++ src/main/java/ru/springBoot/Playlist/controllers/PlaylistController.java
@@ -1,13 +1,19 @@
 package ru.springBoot.Playlist.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.springBoot.Playlist.models.Author;
+import ru.springBoot.Playlist.models.Song;
 import ru.springBoot.Playlist.services.AuthorServices;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequestMapping("/index")
@@ -25,10 +31,14 @@ public class PlaylistController {
         return "/index";
     }
 
-    @GetMapping("{id}")
-    public String oneAuthor(@PathVariable("id") int id, Model model) {
+    @GetMapping("{id}/{pageId}")
+    public String oneAuthor(@PathVariable("id") int id, Model model,
+                            @PathVariable("pageId") int pageId) {
         model.addAttribute("author", authorServices.findOneAuthor(id));
-        model.addAttribute("songs", authorServices.getSongsByAuthorId(id));
+        Map<Integer, List<Song>> map = authorServices.getMapSongsByAuthorId(id);
+        model.addAttribute("currentPage", pageId);
+        model.addAttribute("totalPage", map.size());
+        model.addAttribute("songs", map.get(pageId));
         return "/index_play";
     }
 
@@ -39,9 +49,13 @@ public class PlaylistController {
         return "/index";
     }
 
-    @GetMapping("/all")
-    public String allAuthor(Model model){
-        model.addAttribute("authors", authorServices.findAll());
+    @GetMapping("/all/{pageId}")
+    public String allAuthor(@PathVariable("pageId") int pageId, Model model) {
+        int pageSize = 30;
+        Page<Author> page = authorServices.findPagination(pageId, pageSize);
+        model.addAttribute("currentPage", pageId);
+        model.addAttribute("totalPage", page.getTotalPages());
+        model.addAttribute("authors", page.getContent());
         return "/index";
     }
 }
