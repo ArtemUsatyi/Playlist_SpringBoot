420f1f4c6bbfef3eac623f372be4472826ee6749
artemowork <artem180791@mail.ru>
2023-08-15 16:24:17 +0300

    Исправления по созданию Song, Author реализовано через SongDTO и AuthorDTO. В самом контроллере сделано реализация Конвертирования из SongDTO в Song и для Author

--- src/main/java/ru/springBoot/Playlist/controllers/AdminPanelController.java
+++ src/main/java/ru/springBoot/Playlist/controllers/AdminPanelController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.springBoot.Playlist.dto.AuthorDTO;
+import ru.springBoot.Playlist.dto.SongDTO;
 import ru.springBoot.Playlist.models.Author;
 import ru.springBoot.Playlist.models.Song;
 import ru.springBoot.Playlist.services.AuthorServices;
@@ -62,16 +64,16 @@ public class AdminPanelController {
     }
 
     @GetMapping("/new")
-    public String newAuthor(@ModelAttribute("author") Author author) {
+    public String newAuthor(@ModelAttribute("authorDTO") AuthorDTO authorDTO) {
         return "adminPage/new_author";
     }
 
     @PostMapping
-    public String createAuthor(@ModelAttribute("author") @Valid Author author, BindingResult bindingResult) {
-        authorValidator.validate(author, bindingResult);
+    public String createAuthor(@ModelAttribute("authorDTO") @Valid AuthorDTO authorDTO, BindingResult bindingResult) {
+        authorValidator.validate(authorDTO, bindingResult);
         if (bindingResult.hasErrors()) return "adminPage/new_author";
-        author.setName(author.getName().toUpperCase());
-        authorServices.save(author);
+        authorDTO.setName(authorDTO.getName().toUpperCase());
+        authorServices.save(convertToAuthor(authorDTO));
         return "redirect:/adminPanel";
     }
 
@@ -153,16 +155,16 @@ public class AdminPanelController {
     }
 
     @GetMapping("{id}/newSong")
-    public String newSong(@PathVariable("id") int id, @ModelAttribute("song") Song song, Model model) {
-        song.setAuthor(authorServices.findOneAuthor(id));
-        model.addAttribute("song", song);
+    public String newSong(@PathVariable("id") int id, @ModelAttribute("author") Author author, Model model) {
+        model.addAttribute("author", authorServices.findOneAuthor(id));
+        model.addAttribute("songDTO", new SongDTO());
         return "adminPage/new_song";
     }
 
     @PostMapping("{id}/newSong")
-    public String createSong(@ModelAttribute("song") @Valid Song song, BindingResult bindingResult,
-                             @PathVariable("id") int id, Model model, @RequestParam("file") MultipartFile file) {
-        song.setAuthor(authorServices.findOneAuthor(id));
+    public String createSong(@ModelAttribute("songDTO") @Valid SongDTO songDTO, BindingResult bindingResult,
+                             @PathVariable("id") int id, Model model, @RequestParam("file") MultipartFile file,
+                             @ModelAttribute("author") Author author) {
         if (bindingResult.hasErrors()) return "adminPage/new_song";
         if (file.getSize() > 30000000) {
             model.addAttribute("file", "Максимальный размер файла до 30Мб");
@@ -172,14 +174,14 @@ public class AdminPanelController {
             File uploadDir = new File(uploadPath);
             if (!uploadDir.exists()) uploadDir.mkdir();
             UUID uuid = UUID.randomUUID();
-            song.setLink(uuid + "_" + file.getOriginalFilename());
+            songDTO.setLink(uuid + "_" + file.getOriginalFilename());
             try {
-                file.transferTo(new File(uploadPath + "/" + song.getLink()));
+                file.transferTo(new File(uploadPath + "/" + songDTO.getLink()));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
-        songServices.save(song);
+        songServices.save(convertToPerson(songDTO, id));
         model.addAttribute("author", authorServices.findOneAuthor(id));
         model.addAttribute("songs", authorServices.getSongsByAuthorId(id));
         return "adminPage/list_author_songs";
@@ -197,5 +199,26 @@ public class AdminPanelController {
         model.addAttribute("author", author);
         model.addAttribute("songs", author.getSongs());
         return "adminPage/list_author_songs";
+    }
+    private Song convertToPerson(SongDTO songDTO, int id){
+        Song song = new Song();
+
+        song.setName(songDTO.getName());
+        song.setLink(songDTO.getLink());
+        song.setYearSong(songDTO.getYearSong());
+
+        song.setAuthor(authorServices.findOneAuthor(id));
+
+        return song;
+    }
+
+    private Author convertToAuthor(AuthorDTO authorDTO){
+        Author author = new Author();
+
+        author.setName(authorDTO.getName());
+        author.setAge(authorDTO.getAge());
+        author.setEmail(authorDTO.getEmail());
+
+        return author;
     }
 }
